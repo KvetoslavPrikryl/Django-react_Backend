49a42230c526fa551cd7dfb7d62a86793ab719e1
KvetoslavPrikryl <kvetoslav.prikryl1991@gmail.com>
2023-02-15 12:24:10 +0100

    Secound git backup.

---
+++ Django/Django+React/mynotes/api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+
+@api_view(["GET"])
+def getRoutes(request):
+    return Response("Our API")
+
+@api_view(["GET"])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
+@api_view(["PUT"])
+def updateNote(request, pk):
+    note = Note.objects.get(id=pk)
+    data = request.data
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        
+    return Response(serializer.data)
